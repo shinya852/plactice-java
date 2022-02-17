1bf94abdda79dc166566506c2e24e1a28233deef
shinya852 <shinya.june@gmail.com>
2022-02-17 23:42:56 +0900

    test

---
+++ paiza.io/test.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    System.out.println("こんにちは");
+    System.out.println("Hellow");
+    // erro code
+    // system.out.println("Hellow");
+
+    // 和
+    System.out.println(10 + 5);
+    // 差
+    System.out.println(10 - 5);
+    // 商
+    System.out.println(10 * 5);
+    // 徐
+    System.out.println(10 / 5);
+    // 徐（余り省略）
+    System.out.println(10 / 4);
+    // 小数点
+    System.out.println(10 / 4.0);
+    // 余り
+    System.out.println(10 % 4);
+  }
+}
