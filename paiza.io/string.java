f6ab2ba8f663382773177682179da9a930419f8c
shinya852 <shinya.june@gmail.com>
2022-02-23 23:58:08 +0900

    create note java

---
+++ paiza.io/string.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    // Your code here!
+
+    String name = "いろは"; // nameにいろはを定義
+    System.out.println(name);
+  }
+}
