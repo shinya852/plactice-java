c612f49e52d37ec31a9620efc5bc12cae611828e
shinya852 <shinya.june@gmail.com>
2022-02-24 00:15:08 +0900

    create note java

--- paiza.io/string.java
+++ paiza.io/string.java
@@ -6,5 +6,21 @@ public class Main {
 
     String name = "いろは"; // nameにいろはを定義
     System.out.println(name);
+
+    // 文字列の連結
+    String word1 = "こんにちは";
+    String word2 = "私はJavaです";
+    String word3 = word1 + word2;
+    System.out.println(word3);
+
+    // 文字数表記
+    System.out.println(word3.length());
+
+    // 文字列の取り出し
+    String word = "こんにちは。私はJavaです。";
+    System.out.println(word.substring(6, 12));// 6-11列目を抜き出す
+    System.out.println(word.substring(12)); // 12-末尾を抜き出す
+
+    // 文字列の置き換え
   }
 }
