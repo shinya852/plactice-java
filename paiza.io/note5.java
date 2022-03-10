bb85c8326fe5808977f80974bdeb0021477dacc2
shinya852 <shinya.june@gmail.com>
2022-03-10 13:46:46 +0900

    create note file

---
+++ paiza.io/note5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    // Your code here!
+    // 配列の作り方
+    String[] animal = { "猫", "犬", "亀", "蜥蜴", "鰐", "鯨", "鸚哥", "軍鶏", }; // animalデータに猫ー軍鶏を代入
+    System.out.println(animal[2]);// animal Stringsの中の2番目のデーターを取り出す
+    System.out.println(animal[0]);// データーは1からでなく0番からか順次入力されていく
+  }
+}
