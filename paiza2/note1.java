1655c22481ec834a7597a48e2d1a620f81d4c058
shinya852 <shinya.june@gmail.com>
2022-03-12 20:45:20 +0900

    create note file

---
+++ paiza2/note1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    // Your code here!
+
+    // if文
+    int score = 90; // scoreに90を代入
+    // 以下80点以上は合格と表示
+    if (score >= 80) {
+      System.out.println("合格");
+    }
+  }
+}
